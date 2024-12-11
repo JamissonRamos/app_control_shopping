668654081374848847c2faaf54e7b14dd36bbcbc
Jamisson Lira Ramos <missonlira@gmail.com>
2024-12-11 16:08:22 -0300

    Add or update Firebase GitHub workflows

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -3,17 +3,36 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCUjqWxY389z6vK_uVuVO9Jv6lejkjV-YI",
-    authDomain: "app-control-shopping.firebaseapp.com",
-    projectId: "app-control-shopping",
-    storageBucket: "app-control-shopping.firebasestorage.app",
-    messagingSenderId: "541756049827",
-    appId: "1:541756049827:web:cf744ebb65ea4100077216",
-    measurementId: "G-H53WWR4K5C"
+const firebaseConfigs = {
+    dbPrimary: {
+        apiKey: "AIzaSyCUjqWxY389z6vK_uVuVO9Jv6lejkjV-YI",
+        authDomain: "app-control-shopping.firebaseapp.com",
+        projectId: "app-control-shopping",
+        storageBucket: "app-control-shopping.firebasestorage.app",
+        messagingSenderId: "541756049827",
+        appId: "1:541756049827:web:cf744ebb65ea4100077216",
+        measurementId: "G-H53WWR4K5C"
+    },
+    dbSecondary: {
+        apiKey: "AIzaSyB6o4L4Ofy1FGhAfG4UV7gEnTrGZVNvgQc",
+        authDomain: "app-control-shopping-6b41b.firebaseapp.com",
+        projectId: "app-control-shopping-6b41b",
+        storageBucket: "app-control-shopping-6b41b.firebasestorage.app",
+        messagingSenderId: "794967051218",
+        appId: "1:794967051218:web:667c322bbad382a57e160b",
+        measurementId: "G-EECDKLVGL3"
+    }
 };
 
-// Initialize Firebase
+
+/* 
+    - dbPrimary = Firebase Jamisson;
+    - dbSecondary = Firebase Junior;
+*/
+
+// Inicializa o Firebase baseado no ambiente desejado
+const environment = "dbPrimary"; // ou "dbSecondary"
+const firebaseConfig = firebaseConfigs[environment];
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app)

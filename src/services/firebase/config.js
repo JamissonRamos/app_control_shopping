0e193bcf326b7148927d8f084e6fd15749b7e876
Jamisson Lira Ramos <missonlira@gmail.com>
2024-12-17 09:26:25 -0300

    Update fields

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -4,24 +4,31 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfigs = {
-    dbPrimary: {
-        apiKey: "AIzaSyCUjqWxY389z6vK_uVuVO9Jv6lejkjV-YI",
-        authDomain: "app-control-shopping.firebaseapp.com",
-        projectId: "app-control-shopping",
-        storageBucket: "app-control-shopping.firebasestorage.app",
-        messagingSenderId: "541756049827",
-        appId: "1:541756049827:web:cf744ebb65ea4100077216",
-        measurementId: "G-H53WWR4K5C"
-    },
-    dbSecondary: {
-        apiKey: "AIzaSyB6o4L4Ofy1FGhAfG4UV7gEnTrGZVNvgQc",
-        authDomain: "app-control-shopping-6b41b.firebaseapp.com",
-        projectId: "app-control-shopping-6b41b",
-        storageBucket: "app-control-shopping-6b41b.firebasestorage.app",
-        messagingSenderId: "794967051218",
-        appId: "1:794967051218:web:667c322bbad382a57e160b",
-        measurementId: "G-EECDKLVGL3"
-    }
+    // dbPrimary: {
+    //     apiKey: "AIzaSyCUjqWxY389z6vK_uVuVO9Jv6lejkjV-YI",
+    //     authDomain: "app-control-shopping.firebaseapp.com",
+    //     projectId: "app-control-shopping",
+    //     storageBucket: "app-control-shopping.firebasestorage.app",
+    //     messagingSenderId: "541756049827",
+    //     appId: "1:541756049827:web:cf744ebb65ea4100077216",
+    //     measurementId: "G-H53WWR4K5C"
+    // },
+    // dbSecondary: {
+    //     apiKey: "AIzaSyB6o4L4Ofy1FGhAfG4UV7gEnTrGZVNvgQc",
+    //     authDomain: "app-control-shopping-6b41b.firebaseapp.com",
+    //     projectId: "app-control-shopping-6b41b",
+    //     storageBucket: "app-control-shopping-6b41b.firebasestorage.app",
+    //     messagingSenderId: "794967051218",
+    //     appId: "1:794967051218:web:667c322bbad382a57e160b",
+    //     measurementId: "G-EECDKLVGL3"
+    // }
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 

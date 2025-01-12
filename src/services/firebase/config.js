6dcff072bf863e063d246a52643acefd31023f40
Jamisson Lira Ramos <missonlira@gmail.com>
2025-01-12 10:55:11 -0300

    Atualizando o arquivos para separar prod de dev

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -4,11 +4,18 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Determine o ambiente a partir do .env
-//const environment = import.meta.env.VITE_ENVIRONMENT ;
+const environment = import.meta.env.VITE_ENVIRONMENT || 'development';
 
-//console.log('environment', environment);
 
 const firebaseConfigs = {
+        // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        // projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        // storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        // appId: import.meta.env.VITE_FIREBASE_APP_ID,
+        // measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    development: {
         apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
         authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
         projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -16,45 +23,30 @@ const firebaseConfigs = {
         messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
         appId: import.meta.env.VITE_FIREBASE_APP_ID,
         measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-    // development: {
-    //     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    //     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    //     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    //     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    //     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    //     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    //     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-    // },
-    // production: {
-    //     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    //     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    //     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    //     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    //     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    //     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    //     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-    // }
+    },
+    production: {
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+        measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    }
 };
 
 
-console.log('Firebase Config:', firebaseConfigs);
+console.log('environment', environment);
+console.log('All environment variables:', import.meta.env);
 
-/* 
-    - dev = Firebase Jamisson;
-    - prod = Firebase Junior;
-*/
+console.log('Firebase Config:', firebaseConfigs);
+console.log('Firebase Config [environment]:', firebaseConfigs[environment]);
 
 // Obtenha a configuração correta com base no ambiente
-const config = firebaseConfigs //firebaseConfigs[environment];
+const config = firebaseConfigs[environment];
+
 console.log('config', config);
 
-
-// Verifique se todas as variáveis estão definidas
-//for (const key in config) {
-    if (!config) {
-        throw new Error(`Variável de ambiente não definida para `);
-    }
-//}
 const app = initializeApp(config);
 const analytics = getAnalytics(app);
 const db = getFirestore(app)
